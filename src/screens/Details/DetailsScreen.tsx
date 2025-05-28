049a31c4b99a77846faa007c46e84fb2fc78eb5c
kemal <kemaltaufikf@gmail.com>
2025-05-28 21:09:17 +0700

    11. modal

--- src/screens/Details/DetailsScreen.tsx
+++ src/screens/Details/DetailsScreen.tsx
@@ -4,6 +4,7 @@ import {Text} from 'react-native';
 import CustomButton from '../../components/Button/CustomButton';
 import {globalStyles} from '../../styles/globalStyles';
 import {styles} from './styles';
+import BasicModal from '../../components/BasicModal';
 
 const DetailsScreen: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const DetailsScreen: React.FC = () => {
         title="Alert!"
         onPress={() => Alert.alert('Hello from Details')}
       />
+      <BasicModal />
     </View>
   );
 };
